b8fb9c5d6f0932197b5602cf351f0ec17f016449
王晓丽 <xiaoli.wang@echoesnet.com>
2018-10-23 19:13:33 +0800

    模板

--- src/helper/route.js
+++ src/helper/route.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Handlebars = require('handlebars');
 const path = require('path');
 const config = require('../config/defaultConfig');
+const mime = require('./mime');
 
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
@@ -15,8 +16,9 @@ module.exports = async function(req, res, filePath) {
   try {
     const stats = await stat(filePath);
     if (stats.isFile()) {
+      const contentType = mime(filePath);
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/palin');
+      res.setHeader('Content-Type', contentType);
       fs.createReadStream(filePath).pipe(res);
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath);
@@ -24,12 +26,15 @@ module.exports = async function(req, res, filePath) {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
 
-      const dir = path.relative(config.root,filePath);
+      const dir = path.relative(config.root, filePath);
 
       const data = {
-        files,
+        files: files.map(file => ({
+          file,
+          icon: mime(file)
+        })),
         title: path.basename(filePath),
-        dir: dir ? `/${dir}` :''
+        dir: dir ? `/${dir}` : ''
       };
       res.end(template(data));
     }
