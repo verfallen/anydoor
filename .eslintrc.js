4757e32253b856ebb9b965b66f82253e691f5ce1
王晓丽 <xiaoli.wang@echoesnet.com>
2018-10-23 15:11:42 +0800

    eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,18 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2015
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-console": ["error", { allow: ["info", "warn", "error"] }],
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }]
+  }
 };
