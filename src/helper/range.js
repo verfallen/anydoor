1ec0e14deff094e026389a7078d66d52f8fef25b
王晓丽 <xiaoli.wang@echoesnet.com>
2018-10-24 15:49:25 +0800

    range

---
+++ src/helper/range.js
@@ -0,0 +1,19 @@
+module.exports = (totoalSize, req, res) => {
+  const range = req.headers['range'];
+  if (!range) {
+    return { code: 200 };
+  }
+
+  const sizes = range.match(/bytes=(\d*)-(\d*)/);
+  const end = sizes[2] || totoalSize - 1;
+  const start = sizes[1] || totoalSize - end;
+
+  if (start > end || start < 0 || end > totoalSize) {
+    return { code: 200 };
+  }
+  res.setHeader('Accept-Ranges', 'bytes');
+  res.setHeader('Content-Range', `bytes ${start} -${end}/${totoalSize}`);
+  res.setHeader('Content-Length', end - start);
+
+  return { code: 206, start: parseInt(start), end: parseInt(end) };
+};
--- src/helper/route.js
+++ src/helper/route.js
@@ -5,6 +5,7 @@ const path = require('path');
 const config = require('../config/defaultConfig');
 const mime = require('./mime');
 const compress = require('./compress');
+const range = require('./range');
 
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
@@ -21,7 +22,16 @@ module.exports = async function(req, res, filePath) {
       res.statusCode = 200;
       res.setHeader('Content-Type', contentType);
 
-      let rs = fs.createReadStream(filePath);
+      let rs;
+
+      const { code, start, end } = range(stats.size, req, res);
+      if (code === 200) {
+        rs = fs.createReadStream(filePath);
+      } else {
+        rs = fs.createReadStream(filePath, { start, end });
+      }
+
+      //压缩文件
       if (filePath.match(config.compress)) {
         rs = compress(rs, req, res);
       }
@@ -46,8 +56,9 @@ module.exports = async function(req, res, filePath) {
       res.end(template(data));
     }
   } catch (error) {
+    console.error(error);
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`${filePath} is not a directory or file`);
+    res.end(`${filePath} is not a directory or file!!!`);
   }
 };
